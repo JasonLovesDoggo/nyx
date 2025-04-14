df5526a0afd76a8bd1faf9cfdb21be64d0c63fc3
Jason Cameron <git@jasoncameron.dev>
2025-04-14 14:35:23 -0400

    feat: Enhance post link hover effect with scaling transition and update post description
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { formatDate } from '$lib/utils/date';
+	import { scale } from 'svelte/transition';
 	import type { PostEntry } from '$lib/content/posts';
+	import { backOut } from 'svelte/easing';
 
 	export let data: { posts: PostEntry[] };
 </script>
@@ -15,7 +17,7 @@
 
 	{#if data.posts.length}
 		<div class="space-y-5">
-			{#each data.posts as post (post.slug)}
+			{#each data.posts as post, index (post.slug)}
 				<a
 					href={`/posts/${post.slug}`}
 					class="
@@ -23,10 +25,16 @@
   py-3 no-underline after:absolute
   after:bottom-0 after:left-1/2 after:h-px
   after:w-0 after:-translate-x-1/2
-  after:transition-[width] after:duration-300
-  after:ease-out
-  after:content-[''] hover:after:w-full focus:outline-none
-  focus-visible:ring-2"
+  after:transition-[width]
+  after:duration-400 after:ease-out
+  after:content-[''] hover:after:w-full
+  focus-visible:ring-2
+"
+					in:scale|global={{
+						delay: 250 * index + 100,
+						duration: 750,
+						easing: backOut
+					}}
 				>
 					<div class="flex items-baseline justify-between gap-4">
 						<h2
