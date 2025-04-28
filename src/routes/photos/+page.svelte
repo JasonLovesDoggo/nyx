e852287456bb4763c6fb889e4f724875ccebf3b9
Jason Cameron <git@jasoncameron.dev>
2025-04-27 21:17:41 -0400

    docs: Update page.svelte with out-of-scope note and link to photo gallery
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -1,1 +1,5 @@
-Todo!
+This is out of scope for the ICS Portfolio. I'll write it after the portfolio is done. If you want
+an existing photo gallery, you can check out <a
+	class="text-accent underline underline-offset-4"
+	href="https://jasoncameron.dev/#/photos">My old site</a
+>.
