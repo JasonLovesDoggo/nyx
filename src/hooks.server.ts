886440647c68f89247c160826d461e00a91b4b7a
Jason Cameron <git@jasoncameron.dev>
2025-06-02 20:45:29 -0400

    fix: improve redirect handling by normalizing URL paths
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,8 +4,9 @@ import redirects from '$lib/config/redirects';
 // This hook handles redirects for specific paths to their corresponding URLs.
 // I.E. A redirect from '/github' to Site.out.github or so.
 export const handle: Handle = async ({ event, resolve }) => {
-	const url = event.url;
-	const redirectTo = redirects[url.pathname];
+	const url = event.url.pathname.replace(/\/$/, '');
+	console.log('[REDIRECTS] Handling redirect for:', url);
+	const redirectTo = redirects[url];
 	if (redirectTo) {
 		return new Response(null, {
 			status: 302,
