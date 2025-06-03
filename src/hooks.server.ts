3076d836f215938fc9f693afbb2b4fffda9ccba9
Jason Cameron <git@jasoncameron.dev>
2025-06-02 21:45:50 -0400

    fix: add logging for redirect handling in hooks.server.ts
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,9 +5,9 @@ import redirects from '$lib/config/redirects';
 // I.E. A redirect from '/github' to Site.out.github or so.
 export const handle: Handle = async ({ event, resolve }) => {
 	const url = event.url.pathname.replace(/\/$/, '');
-	console.log('[REDIRECTS] Handling redirect for:', url);
 	const redirectTo = redirects[url];
 	if (redirectTo) {
+		console.log('[REDIRECTS] Handling redirect for:', url);
 		return new Response(null, {
 			status: 302,
 			headers: { Location: redirectTo }
