8d4b9444a4e08cdb368ef98764b69228737b9262
Jason Cameron <jason.cameron@stanwith.me>
2025-10-20 00:06:13 -0400

    fix: update hash calculation to use bitwise OR for improved performance

--- src/components/posts/PostTags.svelte
+++ src/components/posts/PostTags.svelte
@@ -16,7 +16,7 @@
 		let hash = 0;
 		for (let i = 0; i < str.length; i++) {
 			hash = (hash << 5) - hash + str.charCodeAt(i);
-			hash = hash & hash;
+			hash |= 0;
 		}
 		return Math.abs(hash);
 	}
